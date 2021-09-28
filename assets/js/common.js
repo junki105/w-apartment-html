05688ffcda52a4bc449b2f5721b64cf243986eab
unknown <sunlighthan106@gmail.com>
2021-09-29 04:59:40 +0900

    commit before editing top page

--- assets/js/common.js
+++ assets/js/common.js
@@ -11,6 +11,8 @@
     navigationPosition: 'left',
     slidesNavigation: false,
     controlArrows: false,
+    loopHorizontal: true,
+    scrollHorizontally: true,
     anchors: ['hero', 'support', 'philosophy', 'news', 'service', 'case', 'flow', 'blog'],
     menu: '#menu',
     afterLoad: function() {
@@ -24,6 +26,27 @@
       }
     }
   }); 
+  // fullpage customization
+  $('#fullpageTop').fullpage({
+    sectionSelector: '.section',
+    slideSelector: '.slide',
+    navigation: false,
+    slidesNavPosition: 'left',
+    slidesNavigation: true,
+    controlArrows: false,
+    anchors: ['hero', 'support', 'philosophy', 'news', 'service', 'case', 'flow', 'blog'],
+    menu: '#menu',
+    afterSlideLoad: function() {
+      var slideNumber = $('.fp-slide.active').index() + 1;
+      if (slideNumber > 9) {
+        $('#num').html(slideNumber);
+      }
+      else {
+        //This will change the input of NUM to current section number
+        $('#num').html("0" + slideNumber);
+      }
+    }
+  });
   $(document).on('click', '#moveRight', function(){
     fullpage_api.moveSlideRight();
   });
@@ -31,7 +54,7 @@
     fullpage_api.moveSlideLeft();
   });
   $(document).on('click', '#moveDown', function(){
-    fullpage_api.moveSectionDown();
+    fullpage_api.moveSlideRight();
   });
 
 })(jQuery);
